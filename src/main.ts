7d2fb64442fee162fcc51218a768a4f30ecad307
Hung <nguyenthinhhung150603@gmail.com>
2025-12-11 15:27:28 +0700

    video 22.1

--- src/main.ts
+++ src/main.ts
@@ -6,5 +6,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   await app.listen(configService.get<string>('PORT') || 3000);
+
+  console.log(
+    `Application is running on: ${configService.get<string>('PORT')}`,
+  );
 }
 bootstrap();
