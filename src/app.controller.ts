3ec31b46de950e3fba7b8eb18886b0c835095ef3
Hung <nguyenthinhhung150603@gmail.com>
2025-12-30 14:24:53 +0700

    56

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,6 @@
 import { ConfigService } from '@nestjs/config';
 import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
-import { LocalAuthGuard } from './auth/local-auth.guard';
-import { AuthService } from './auth/auth.service';
-import { JwtAuthGuard } from './auth/jwt-auth.guard';
-import { Public } from './decorator/customize';
 
 @Controller()
 export class AppController {
