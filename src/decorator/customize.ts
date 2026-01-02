a67699015da18a3b186d7fabda75a3ce374f83c4
Hung <nguyenthinhhung150603@gmail.com>
2026-01-02 18:04:06 +0700

    61

--- src/decorator/customize.ts
+++ src/decorator/customize.ts
@@ -1,7 +1,16 @@
-import { createParamDecorator, ExecutionContext, SetMetadata } from '@nestjs/common';
+import {
+  createParamDecorator,
+  ExecutionContext,
+  SetMetadata,
+} from '@nestjs/common';
 
 export const IS_PUBLIC_KEY = 'isPublic';
 export const Public = () => SetMetadata(IS_PUBLIC_KEY, true);
+
+export const RESPONSE_MESSAGE = 'response_message';
+
+export const ResponseMessage = (message: string) =>
+  SetMetadata(RESPONSE_MESSAGE, message);
 
 export const User = createParamDecorator(
   (data: unknown, ctx: ExecutionContext) => {
