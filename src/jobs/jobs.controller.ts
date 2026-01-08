68674c69e78df381e065fda9c7aa729f83846154
Hung <nguyenthinhhung150603@gmail.com>
2026-01-08 19:33:53 +0700

    api-full-crud-jobs

--- src/jobs/jobs.controller.ts
+++ src/jobs/jobs.controller.ts
@@ -6,6 +6,7 @@ import {
   Patch,
   Param,
   Delete,
+  Query,
 } from '@nestjs/common';
 import { JobsService } from './jobs.service';
 import { CreateJobDto } from './dto/create-job.dto';
@@ -24,13 +25,19 @@ export class JobsController {
   }
 
   @Get()
-  findAll() {
-    return this.jobsService.findAll();
+  @ResponseMessage('Fetch jobs with pagination')
+  findAll(
+  @Query('current') currentPage: string,
+    @Query('pageSize') limit: string,
+    @Query() qs: string,
+  ) {
+    return this.jobsService.findAll(+currentPage, +limit, qs);
   }
 
   @Get(':id')
+  @ResponseMessage('Fetch a job by id')
   findOne(@Param('id') id: string) {
-    return this.jobsService.findOne(+id);
+    return this.jobsService.findOne(id);
   }
 
   @Patch(':id')
@@ -44,7 +51,8 @@ export class JobsController {
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.jobsService.remove(+id);
+  @ResponseMessage('Delete a job successfully')
+  remove(@Param('id') id: string, @User() user: IUser) {
+    return this.jobsService.remove(id, user);
   }
 }
--- src/jobs/jobs.service.ts
+++ src/jobs/jobs.service.ts
@@ -5,6 +5,7 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Job, JobDocument } from './schemas/job.schema';
 import type { SoftDeleteModel } from 'soft-delete-plugin-mongoose';
 import { IUser } from 'src/users/users.interface';
+import aqp from 'api-query-params';
 
 @Injectable()
 export class JobsService {
@@ -26,22 +27,51 @@ export class JobsService {
     };
   }
 
-  findAll() {
-    return `This action returns all jobs`;
+  async findAll(currentPage: number, limit: number, qs: string) {
+    const { filter, sort, population } = aqp(qs);
+    delete filter.current;
+    delete filter.pageSize;
+    let offset = (+currentPage - 1) * +limit;
+    let defaultLimit = +limit ? +limit : 10;
+
+    const totalItems = (await this.jobModel.find(filter)).length;
+    const totalPages = Math.ceil(totalItems / defaultLimit);
+
+    const result = await this.jobModel
+      .find(filter)
+      .skip(offset)
+      .limit(defaultLimit)
+      .sort(sort as any)
+      .populate(population)
+      .exec();
+
+    return {
+      meta: {
+        current: currentPage, //trang hiện tại
+        pageSize: limit, //số lượng bản ghi đã lấy
+        pages: totalPages, //tổng số trang với điều kiện query
+        total: totalItems, // tổng số phần tử (số bản ghi)
+      },
+      result, //kết quả query
+    };
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} job`;
+  findOne(id: string) {
+    return this.jobModel.findById(id).exec();
   }
 
-  update(id: string, updateJobDto: UpdateJobDto , user: IUser) {
+  update(id: string, updateJobDto: UpdateJobDto, user: IUser) {
     return this.jobModel.updateOne(
       { _id: id },
       { ...updateJobDto, updatedBy: { _id: user._id, email: user.email } },
     );
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} job`;
+  async remove(id: string, user: IUser) {
+    await this.jobModel.updateOne(
+      { _id: id },
+      { deletedBy: { _id: user._id, email: user.email } },
+    );
+    return this.jobModel.softDelete({ _id: id });
   }
 }
